753f88eb4f46d82b7e1ff3f4835c3e59becfe770
Stephen K <zero298@gmail.com>
2015-08-18 13:18:44 -0500

    Exposed signal enum to public and linted index
    - Exposed the signal enumeration so that you can check the signal you
    are reading
    - Made the index.js file jslint compliant

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/*global process */
+
+"use strict";
+
 var morse = require("./src/morsejs"),
     toTranslate = (process.argv[2] || null);
 
-if(toTranslate){
+if (toTranslate) {
     console.log(morse.translate(toTranslate));
-}
-else {
+} else {
     console.log("Please provide a message to translate");
 }
