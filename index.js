c18a92d5257f43bcda81f521e50c8bd6f04f38ae
Stephen K <zero298@gmail.com>
2016-01-19 15:07:53 -0600

    Added error codes and man page
    + Shell script will exit with error code if message is not given
    + Added a man page to be cool and hip

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 
 "use strict";
 
-console.log("dirname: " + __dirname);
-
 var morse = require("./src/morsejs"),
     toTranslate = (process.argv[2] || null);
 
 if (toTranslate) {
     console.log(morse.translate(toTranslate));
 } else {
-    console.log("Please provide a message to translate");
+    console.error("Please provide a message to translate");
+    process.exit(1);
 }
